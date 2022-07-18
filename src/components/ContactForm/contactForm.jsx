c87e3bbd2e8bbe72faaf4ea4044642c19f9405c2
EduardSergienko <katleraster@gmail.com>
2022-07-18 10:36:56 +0300

    Update contactForm.jsx

--- src/components/ContactForm/contactForm.jsx
+++ src/components/ContactForm/contactForm.jsx
@@ -1,66 +1,75 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './contactForm.module.scss';
 
-export default class ContactForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  // static propTypes = {
+  //   onSubmit: PropTypes.func.isRequired,
+  // };
 
-  state = {
-    name: '',
-    number: '',
-  };
+  // state = {
+  //   name: '',
+  //   number: '',
+  // };
 
-  onInputtype = e => {
+  const onInputtype = e => {
     const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
-  onSubmiteForm = e => {
+  const onSubmiteForm = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit(name, number);
+    reset();
   };
 
-  reset = () => {
+  const reset = () => {
     this.setState({ name: '', number: '' });
   };
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={styles.contactForm} onSubmit={this.onSubmiteForm}>
-        <label>
-          <p className={styles.contactForm__label}>Name</p>
-          <input
-            className={styles.contactForm__input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.onInputtype}
-          />
-        </label>
-        <label>
-          <p className={styles.contactForm__label}>Phone</p>
-          <input
-            className={styles.contactForm__input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.onInputtype}
-          />
-        </label>
-        <button className={styles.contactForm__btn} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={styles.contactForm} onSubmit={onSubmiteForm}>
+      <label>
+        <p className={styles.contactForm__label}>Name</p>
+        <input
+          className={styles.contactForm__input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={onInputtype}
+        />
+      </label>
+      <label>
+        <p className={styles.contactForm__label}>Phone</p>
+        <input
+          className={styles.contactForm__input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={onInputtype}
+        />
+      </label>
+      <button className={styles.contactForm__btn} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+
+  // const { name, number } = this.state;
 }
